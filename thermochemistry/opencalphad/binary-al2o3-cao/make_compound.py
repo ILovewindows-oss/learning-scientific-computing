c3b78112e14eb8754ea9f63531f09b7bda0eb42a
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2022-07-04 16:51:15 +0200

    Reworked macros

---
+++ thermochemistry/opencalphad/binary-al2o3-cao/make_compound.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+def compute_atomic_fraction(x_cor, x_cao):
+    """ Compute atomic fractions to use in calculations. """
+    total = 5 * x_cor + 2 * x_cao
+
+    x_al = 2 * x_cor / total
+    x_ca = 1 * x_cao / total
+
+    print(f"*** COMPOUND C({x_cao}).A({x_cor})")
+    print(f"SET CONDITION X(AL)={x_al:.5f}")
+    print(f"SET CONDITION X(CA)={x_ca:.5f}")
+    print("\n")
+
+
+compute_atomic_fraction(0.0, 1.0)
+compute_atomic_fraction(1.0, 3.0)
+compute_atomic_fraction(1.0, 1.0)
+compute_atomic_fraction(2.0, 1.0)
+compute_atomic_fraction(6.0, 1.0)
+compute_atomic_fraction(1.0, 0.0)
+
+# Between CAO and C3A1
+compute_atomic_fraction(0.9, 3.0)
+
+# Between C3A1 and C1A1
+compute_atomic_fraction(0.9, 1.0)
+
+# Between C1A1 and C1A2
+compute_atomic_fraction(1.9, 1.0)
+
+# Between C1A2 and C1A6
+compute_atomic_fraction(6.0, 0.9)
+
+# Between C1A6 and COR
+compute_atomic_fraction(1.0, 0.1)
--- thermochemistry/opencalphad/binary-al2o3-cao/runAll.sh
+++ thermochemistry/opencalphad/binary-al2o3-cao/runAll.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
-rm -rf *.LOG *.plt *.png
+rm -rf dumps/*.LOG dumps/*.plt dumps/*.png
 
+# OK
 oc6P macro-01-unary-al2o3.ocm
+
+# OK
 oc6P macro-02-unary-cao.ocm
+
+# OK
+oc6P macro-03-unary-c3a1.ocm
+
+# NOK
+# oc6P macro-04-unary-c1a1.ocm
+
+# NOK
+# oc6P macro-05-unary-c1a2.ocm
+
+# OK
+oc6P macro-06-unary-c1a6.ocm
+
+# Needs improvement, very bad but...
+oc6P macro-07-binary-cao-c3a1.ocm
+
+# Needs improvement, very bad but... better than 7
+oc6P macro-08-binary-c3a1-c1a1.ocm
+
+# Needs improvement, very bad but... better than 7
+oc6P macro-09-binary-c1a1-c1a2.ocm
+
+# OK
+oc6P macro-10-binary-c1a2-c1a6.ocm
+
+# OK
+oc6P macro-11-binary-c1a6-al2o3.ocm
