b93253d0d1f39faa1f44b94330c0528640fb85cb
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2022-07-09 12:48:07 +0200

    Added reworked tutorial

---
+++ finite-elements/kratos-multiphysics/02-CHT-Cylinder_Cooling_Re100_Pr2/run.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+import KratosMultiphysics
+import KratosMultiphysics.FluidDynamicsApplication
+import KratosMultiphysics.ConvectionDiffusionApplication
+
+from KratosMultiphysics\
+    .ConvectionDiffusionApplication\
+    .convection_diffusion_analysis import ConvectionDiffusionAnalysis
+
+
+class ConvectionDiffusionAnalysisWithFlush(ConvectionDiffusionAnalysis):
+    """ Add STDOUT flushing to problem analysis for better screen output. """
+    def __init__(self,model, project_parameters, flush_frequency=10.0):
+        super(ConvectionDiffusionAnalysisWithFlush, self).__init__(model,project_parameters)
+
+        self.flush_frequency = flush_frequency
+        self.last_flush = time.time()
+
+    def FinalizeSolutionStep(self):
+        super(ConvectionDiffusionAnalysisWithFlush, self).FinalizeSolutionStep()
+
+        if self.parallel_type == "OpenMP":
+            now = time.time()
+            if now - self.last_flush > self.flush_frequency:
+                sys.stdout.flush()
+                self.last_flush = now
+
+
+if __name__ == "__main__":
+    with open("settings/parameters.json",'r') as parameter_file:
+        parameters = KratosMultiphysics.Parameters(parameter_file.read())
+
+    model = KratosMultiphysics.Model()
+    simulation = ConvectionDiffusionAnalysisWithFlush(model, parameters)
+    simulation.Run()
