e525f976bb6bb4f11f81c4ee5622ad69cf853049
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2022-07-18 06:29:58 +0200

    Added exercise

---
+++ base-skills/manim/03-square-and-circle/scene.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+""" Run with `manim -pql scene.py SquareAndCircle`. """
+# Manim base class.
+from manim import Scene
+
+# Manim actions.
+from manim import Create
+
+# Manim shapes.
+from manim import Circle
+from manim import Square
+
+# Manim constants.
+from manim import BLUE
+from manim import PINK
+from manim import RIGHT
+
+
+class SquareAndCircle(Scene):
+    """ Scene for converting a square into a circle. """
+    def construct(self):
+        """ Construct and display the animation. """
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.6)
+
+        square.next_to(circle, RIGHT, buff=0.5)
+        self.play(Create(circle), Create(square))
