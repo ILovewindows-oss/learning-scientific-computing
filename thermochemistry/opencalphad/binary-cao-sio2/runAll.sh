94fd5ba8cc4587ae6cb5f32bf4b48b61dfca9995
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2022-08-16 17:56:48 +0200

    Failed experimentation with database

--- thermochemistry/opencalphad/binary-cao-sio2/runAll.sh
+++ thermochemistry/opencalphad/binary-cao-sio2/runAll.sh
@@ -4,9 +4,16 @@ rm -rf dumps/*.LOG dumps/*.plt dumps/*.png
 OCEXE="mpiexec -np 4 oc6P"
 
 # OK
-${OCEXE} macro-01-unary-cao.ocm
+# ${OCEXE} macro-01-unary-cao.ocm
 
 # OK
-${OCEXE} macro-02-unary-sio2.ocm
+# ${OCEXE} macro-02-unary-cao.ocm
 
+# NOK
+# ${OCEXE} macro-03-unary-sio2.ocm
 
+# NOK
+# ${OCEXE} macro-04-unary-sio2.ocm
+
+# NOK
+# ${OCEXE} macro-05-unary-sio2.ocm
