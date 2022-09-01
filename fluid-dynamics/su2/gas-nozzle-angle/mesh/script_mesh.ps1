9636fc782912fecf3628e766a17a9470b3cefcc9
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2022-09-01 09:27:56 +0200

    Minor changes

--- fluid-dynamics/su2/gas-nozzle-angle/mesh/script_mesh.ps1
+++ fluid-dynamics/su2/gas-nozzle-angle/mesh/script_mesh.ps1
@@ -1,10 +1,11 @@
-gmsh.exe nozzle_2d.geo -2 -format cgns
-gmsh.exe nozzle_2d.geo -2 -format su2
+# Note: it is extruded 1-cell thick, thus 3D.
+
+gmsh.exe nozzle_2d.geo -3 -format cgns
+gmsh.exe nozzle_2d.geo -3 -format su2
 
 gmsh.exe nozzle_3d.geo -3 -format cgns
 gmsh.exe nozzle_3d.geo -3 -format su2
 
-# Note: it is extruded 1-cell thick, thus 3D.
 gmsh.exe domain_2d.geo -3 -format cgns
 gmsh.exe domain_2d.geo -3 -format su2
 
--- fluid-dynamics/su2/gas-nozzle-angle/mesh/script_mesh.sh
+++ fluid-dynamics/su2/gas-nozzle-angle/mesh/script_mesh.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-gmsh nozzle_2d.geo -2 -format cgns
-gmsh nozzle_2d.geo -2 -format su2
+# Note: it is extruded 1-cell thick, thus 3D.
+
+gmsh nozzle_2d.geo -3 -format cgns
+gmsh nozzle_2d.geo -3 -format su2
 
 gmsh nozzle_3d.geo -3 -format cgns
 gmsh nozzle_3d.geo -3 -format su2
 
-# Note: it is extruded 1-cell thick, thus 3D.
 gmsh domain_2d.geo -3 -format cgns
 gmsh domain_2d.geo -3 -format su2
 
