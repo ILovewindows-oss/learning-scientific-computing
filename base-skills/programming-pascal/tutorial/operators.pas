b9dc539f54b658a06becd7c69fee44847d4b14d4
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2022-11-24 16:33:43 +0100

    Added content

--- base-skills/programming-pascal/tutorial/operators.pas
+++ base-skills/programming-pascal/tutorial/operators.pas
@@ -1,6 +1,52 @@
 program operators;
 uses crt;
 
+var
+    a: real;
+    b: real;
+    c: real;
+    d: integer;
+    e: integer;
+    f: integer;
+
 begin
+    a := 2;
+    b := 3;
+    d := 5;
+    e := 3;
     
+    writeln('**** INITIALIZED VARIABLES');
+    writeln('a = ', a:5:3);
+    writeln('b = ', b:5:3);
+    writeln('d = ', d);
+    writeln('e = ', e);
+
+    writeln('**** ARITHMETIC OPERATORS');
+
+    c := a + b;
+    writeln('c = a + b   >> ', c:6:3);
+
+    c := a - b;
+    writeln('c = a - b   >> ', c:6:3);
+
+    c := a * b;
+    writeln('c = a * b   >> ', c:6:3);
+
+    c := a / b;
+    writeln('c = a / b   >> ', c:6:3);
+
+    f := d div e;
+    writeln('f = d div e >> ', f);
+
+    f := d mod e;
+    writeln('f = d mod e >> ', f);
+
+    writeln('**** RELATIONAL OPERATORS');
+    writeln('d = e   >> ', d = e);
+    writeln('d <> e  >> ', d <> e);
+    writeln('d > e   >> ', d > e);
+    writeln('d < e   >> ', d < e);
+    writeln('d >= e  >> ', d >= e);
+    writeln('d <= e  >> ', d <= e);
+
 end.
